47c5b51fc3bfb199ff03eb734933c90ea99e1857
coldrain-f <intmain__@naver.com>
2022-06-04 16:52:47 +0900

    Initial commit

---
+++ src/main/java/edu/coldrain/spe/SpringbootSpeApplication.java
@@ -0,0 +1,13 @@
+package edu.coldrain.spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/coldrain/spe/SpringbootSpeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coldrain.spe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
